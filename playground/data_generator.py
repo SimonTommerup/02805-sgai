aaec7d9bc63557f165090d86f65c791d9964ddd4
Simon Ankjær Tommerup <sito@ihfood.dk>
2020-11-04 16:25:29 +0100

    update data_generator

--- playground/data_generator.py
+++ playground/data_generator.py
@@ -7,18 +7,24 @@ dataset = pd.read_csv("./../dataset.txt")
 
 subreddits = [r.subreddit("trump")]
 for s in subreddits:
-    # top threads
-    s_top = s.top(limit=num_threads)
 
-    # remove MoreComments
+    s_top = s.top(limit=num_threads)
     for thread in s_top:
         thread.comments.replace_more(limit=0)
 
         comments = thread.comments
         for comment in comments[:1]:
-            #dataset["user"] = comment.author
-            #dataset["comment"] = comment.body
-            print(comment.author)
-            #print(dir(comment.author))
-            print(dir(reddit.redditor(comment.author)))
+            ## check that this works as intended
+            dataset["user"] = comment.author
+            dataset["comment"] = comment.body
+            
+            user = reddit.redditor(str(comment.author))
+            used_subreddits = []
+            for user_comment in user.comments.top(limit=100):
+                used_subreddit = user_comment.subreddit
+                if not user_comment.subreddit in used_subreddits:
+                    used_subreddits.append(used_subreddit)
+
+
+
 # %%
