e7053b80499c5b589ab0b7135e90bc1f25deb3f1
Simon Ankjær Tommerup <sito@ihfood.dk>
2020-11-04 19:33:59 +0100

    clean up

--- playground/data_generator.py
+++ playground/data_generator.py
@@ -6,9 +6,7 @@ from prawcore.exceptions import Forbidden
 num_threads = 1
 num_comments_per_thread = 1
 users_to_skip = [None, "AutoModerator"]
-
 data = []
-
 main_subreddits = [reddit.subreddit("trump"), reddit.subreddit("JoeBiden")]
 main_titles = [s.title for s in main_subreddits]
 
@@ -24,36 +22,17 @@ for subreddit in main_subreddits:
             if comment.author not in users_to_skip:
                 user = reddit.redditor(comment.author.name)
                 used_subreddits = []
-
+                
                 try:
                     for user_comment in user.comments.top(limit=10):
-                        print("Looping")
                         used_subreddit = user_comment.subreddit.title
                         if not used_subreddit in main_titles:
                             if not used_subreddit in used_subreddits:
-                                print("Add")
                                 used_subreddits.append(used_subreddit)
 
                 except Forbidden:
-                    print("Except")
                     continue
-
-                data_item = [comment.author.name,subreddit.title,comment.body,used_subreddits,None]
-                data.append(data_item)
-
-#%%
-# Pandas DataFrame
-columns = ["user","from_subreddit","comment","used_subreddits" ,"comment_sentiment"]
-df = pd.DataFrame(data=data, columns=columns)
-
-print(len(df))
-df
-
-
-# %%
-#df.to_csv("./../testdata2.csv",sep=";",columns=columns,index=False)
-# %%
-
-
-print(df["used_subreddits"][0])
-# %%
+                
+                if not len(used_subreddits) < 1:
+                    data_item = [comment.author.name,subreddit.title,comment.body,used_subreddits,None]
+                    data.append(data_item)
