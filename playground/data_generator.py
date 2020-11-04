1a8ce71e00247877f15011878d48589051b27afb
Simon Ankjær Tommerup <sito@ihfood.dk>
2020-11-04 17:27:33 +0100

    data_generator.py update

--- playground/data_generator.py
+++ playground/data_generator.py
@@ -1,30 +1,40 @@
 #%%
 import pandas as pd
-from reddit import reddit as r
+from reddit import reddit
+from prawcore.exceptions import Forbidden
 
-num_threads=1
-dataset = pd.read_csv("./../dataset.txt")
+num_threads = 1
+num_comments_per_thread = 5
+data = []
 
-subreddits = [r.subreddit("trump")]
-for s in subreddits:
+subreddits = [reddit.subreddit("trump")]
 
-    s_top = s.top(limit=num_threads)
-    for thread in s_top:
+for subreddit in subreddits:
+    subreddit_threads = subreddit.top(limit=num_threads)
+
+    for thread in subreddit_threads:
         thread.comments.replace_more(limit=0)
 
         comments = thread.comments
-        for comment in comments[:1]:
-            ## check that this works as intended
-            dataset["user"] = comment.author
-            dataset["comment"] = comment.body
-            
-            user = reddit.redditor(str(comment.author))
-            used_subreddits = []
-            for user_comment in user.comments.top(limit=100):
-                used_subreddit = user_comment.subreddit
-                if not user_comment.subreddit in used_subreddits:
-                    used_subreddits.append(used_subreddit)
+        for comment in comments[:num_comments_per_thread]:
+            if comment.author is not None:
+                user = reddit.redditor(comment.author.name)
+                used_subreddits = []
+                try:
+                    for user_comment in user.comments.top(limit=100):
+                        used_subreddit = user_comment.subreddit
+                        if not user_comment.subreddit in used_subreddits:
+                            used_subreddits.append(used_subreddit)
+                except Forbidden:
+                    continue
 
+                data_item = [comment.author.name,subreddit.title,comment.body,used_subreddits,None]
+                data.append(data_item)
 
+#%%
+# Pandas DataFrame
+columns = ["user","from_subreddit","comment","used_subreddits" ,"comment_sentiment"]
+df = pd.DataFrame(data=data, columns=columns)
 
 # %%
+pron
--- playground/initial_test.py
+++ playground/initial_test.py
@@ -1,50 +1,14 @@
 #%%
-import praw
-import json
+import pandas as pd
 
-jcred = open("./../rcred.json")
-rcred = json.load(jcred)
-jcred.close()
-
-# Reddit instance
-r = praw.Reddit(
-                client_id=rcred["id"],
-                client_secret=rcred["secret"],
-                username=rcred["user"],
-                password=rcred["pass"],
-                user_agent=rcred["agent"]
-                )
-
-# Verify authentication
-#print(r.user.me())
-#print(r.read_only)
-
-# Subreddit instance
-s = r.subreddit("trump")
-
-# new threads
-s_new = s.top(limit=1)
-
-# the threads object is a generator, 
-# so to yield one, we can use iter 
-thread = next(iter(s_new))
-
-# remove MoreComments objects from CommentsForest object
-# by setting replace_more(limit=0)
-thread.comments.replace_more(limit=0)
-
-# print example comment
-#print(thread.comments[5].body)
-
-print(thread.comments[5].author)
+df = pd.DataFrame
+with open("./../dataset.txt","a") as f:
+    pd.d = pd.to_csv(f, mode="a")
 
 
 
-# Comments object
-#c = s.comments
-#print(c.parse())
 
 
-## %%
+
 
 # %%
