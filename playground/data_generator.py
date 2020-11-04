9582562988c726a8446ecd3b12cbc7308c5d8405
Simon Ankjær Tommerup <sito@ihfood.dk>
2020-11-04 19:39:50 +0100

    minor changes

--- playground/data_generator.py
+++ playground/data_generator.py
@@ -6,7 +6,10 @@ from prawcore.exceptions import Forbidden
 num_threads = 1
 num_comments_per_thread = 1
 users_to_skip = [None, "AutoModerator"]
+file_name = "TESTDATA_01.csv"
+
 data = []
+
 main_subreddits = [reddit.subreddit("trump"), reddit.subreddit("JoeBiden")]
 main_titles = [s.title for s in main_subreddits]
 
@@ -14,15 +17,14 @@ for subreddit in main_subreddits:
 
     subreddit_threads = subreddit.top(limit=num_threads)
     for thread in subreddit_threads:
-        thread.comments.replace_more(limit=0)
 
+        thread.comments.replace_more(limit=0)
         comments = thread.comments
         for comment in comments[:num_comments_per_thread]:
 
             if comment.author not in users_to_skip:
                 user = reddit.redditor(comment.author.name)
                 used_subreddits = []
-                
                 try:
                     for user_comment in user.comments.top(limit=10):
                         used_subreddit = user_comment.subreddit.title
@@ -36,3 +38,9 @@ for subreddit in main_subreddits:
                 if not len(used_subreddits) < 1:
                     data_item = [comment.author.name,subreddit.title,comment.body,used_subreddits,None]
                     data.append(data_item)
+
+#%%
+# Pandas DataFrame
+columns = ["user","from_subreddit","comment","used_subreddits" ,"comment_sentiment"]
+df = pd.DataFrame(data=data, columns=columns)
+df.to_csv(f"./../{file_name}",sep=";",columns=columns,index=False)
