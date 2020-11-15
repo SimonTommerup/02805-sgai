f45080aeb0e8d8705ca0d0247280d632c3ad4dec
Simon Ankjær Tommerup <sito@ihfood.dk>
2020-11-15 22:19:23 +0100

    added join_data_partitions

--- project/utils.py
+++ project/utils.py
@@ -71,6 +71,16 @@ def load_ids_from_pickle(partition, main=False):
         ids = pickle.load(handle)
     return ids
 
+def join_data_partitions(dataframes):
+    for df in dataframes:
+        idx = df[df["user"]=="user"].index
+        if len(idx) > 0:
+                df = df.drop(idx.values)
+    joined_df = dataframes[0]
+    for df in dataframes[1:]:
+        joined_df = joined_df.append(df)
+    return joined_df
+
 def remove_repeated_user_entries(dataframe):
     df = dataframe
     seen_users = []
